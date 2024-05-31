1145681a9923aca6a3d23ac9316231603f436555
EvgheniSergheev <evgheni@optimum-web.com>
2024-05-31 14:59:33 +0300

    trigger

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -4,7 +4,6 @@ import {mount} from 'marketing/MarketingIndex';
 export default () => {
     const ref = useRef(null);
     useEffect(() => {
-        console.log(mount);
         mount(ref.current);
     })
     return <div ref={ref} />
