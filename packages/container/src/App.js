fe321b576b17d693d76a46798cf933847c616e6b
EvgheniSergheev <evgheni@optimum-web.com>
2024-05-31 16:04:11 +0300

    css fix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generatedClassNames = createGenerateClassName({
+    prodctionPrefix: 'co'
+})
 
 export default () => {
   return (
-    <BrowserRouter>
-        <div>
-            <Header />
-            <MarketingApp />
-        </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generatedClassNames}>
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
+    </StylesProvider>
   )
 };
