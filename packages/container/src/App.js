06bfece5be494ae2d0bac4e36a6c6e0715324b17
EvgheniSergheev <evgheni@optimum-web.com>
2024-05-31 15:16:24 +0300

    trigger

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
+    console.log('container');
+    
   return (
     <MarketingApp />
   );
